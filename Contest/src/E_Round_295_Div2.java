cb1c1c53b69444ec34cc1ce17054012f5f6620ee
trungpham90 <trungpham90@gmail.com>
2015-05-19 01:08:34 +0800

    E Round 295 Div 2

---
+++ Contest/src/E_Round_295_Div2.java
@@ -0,0 +1,314 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ * #
+ *
+ * @author pttrung
+ */
+public class E_Round_295_Div2 {
+
+    public static long MOD = 1000000007;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // PrintWriter out = new PrintWriter(new FileOutputStream(new File(
+        // "output.txt")));
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner in = new Scanner();
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        String line = in.next();
+        if (k == 0) {
+            BigInteger result = new BigInteger(line);
+            result = result.mod(BigInteger.valueOf(MOD));
+            out.println(result.longValue());
+        } else {
+            long result = 0;
+            int max = n - k;
+            BigInteger start = BigInteger.ONE;
+            int cur = 1;
+            BigInteger mod = BigInteger.valueOf(MOD);
+            long[] data = new long[max + 1];
+
+            for (int i = max; i > 0; i--) {
+                data[i] = start.longValue();
+                start = start
+                        .multiply(
+                        BigInteger.valueOf((long) (k - 1 + cur))
+                        .multiply(
+                        BigInteger.valueOf((long) cur)
+                        .modInverse(mod))).mod(
+                        mod);
+                cur++;
+            }
+            long[] sum = new long[max + 1];
+            sum[max] = data[max];
+            for (int i = max - 1; i > 0; i--) {
+                sum[i] = data[i] + sum[i + 1];
+                sum[i] %= MOD;
+            }
+            long add = 0;
+            for (int i = 1; i <= max; i++) {
+                long tmp = pow(10, i - 1);
+                add += (tmp * data[i]) % MOD;
+                add %= MOD;
+                data[i] = add + (i + 1 <= max ? (sum[i + 1] * tmp) % MOD : 0);
+                data[i] %= MOD;
+            }
+            for (int i = 0; i < n; i++) {
+                long digit = line.charAt(i) - '0';
+                if (n - i >= max) {
+                    result += (digit * data[max]) % MOD;
+                } else {
+                    result += (digit * data[n - i]) % MOD;
+                }
+                result %= MOD;
+            }
+
+            out.println(result);
+        }
+        out.close();
+    }
+
+    public static int[] KMP(String val) {
+        int i = 0;
+        int j = -1;
+        int[] result = new int[val.length() + 1];
+        result[0] = -1;
+        while (i < val.length()) {
+            while (j >= 0 && val.charAt(j) != val.charAt(i)) {
+                j = result[j];
+            }
+            j++;
+            i++;
+            result[i] = j;
+        }
+        return result;
+
+    }
+
+    public static long pow(int val, int p) {
+        if (p == 0) {
+            return 1;
+        }
+        long v = pow(val, p / 2);
+        if (p % 2 == 0) {
+            return (v * v) % MOD;
+        } else {
+            return (((v * val) % MOD) * v) % MOD;
+        }
+    }
+
+    public static boolean nextPer(int[] data) {
+        int i = data.length - 1;
+        while (i > 0 && data[i] < data[i - 1]) {
+            i--;
+        }
+        if (i == 0) {
+            return false;
+        }
+        int j = data.length - 1;
+        while (data[j] < data[i - 1]) {
+            j--;
+        }
+        int temp = data[i - 1];
+        data[i - 1] = data[j];
+        data[j] = temp;
+        Arrays.sort(data, i, data.length);
+        return true;
+    }
+
+    public static int digit(long n) {
+        int result = 0;
+        while (n > 0) {
+            n /= 10;
+            result++;
+        }
+        return result;
+    }
+
+    public static double dist(long a, long b, long x, long y) {
+        double val = (b - a) * (b - a) + (x - y) * (x - y);
+        val = Math.sqrt(val);
+        double other = x * x + a * a;
+        other = Math.sqrt(other);
+        return val + other;
+
+    }
+
+    public static class Point implements Comparable<Point> {
+
+        int x, y;
+
+        public Point(int start, int end) {
+            this.x = start;
+            this.y = end;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 5;
+            hash = 47 * hash + this.x;
+            hash = 47 * hash + this.y;
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final Point other = (Point) obj;
+            if (this.x != other.x) {
+                return false;
+            }
+            if (this.y != other.y) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return x - o.x;
+        }
+    }
+
+    public static class FT {
+
+        long[] data;
+
+        FT(int n) {
+            data = new long[n];
+        }
+
+        public void update(int index, long value) {
+            while (index < data.length) {
+                data[index] += value;
+                index += (index & (-index));
+            }
+        }
+
+        public long get(int index) {
+            long result = 0;
+            while (index > 0) {
+                result += data[index];
+                index -= (index & (-index));
+            }
+            return result;
+
+        }
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static long pow(long a, long b) {
+        if (b == 0) {
+            return 1;
+        }
+        if (b == 1) {
+            return a;
+        }
+        long val = pow(a, b / 2);
+        if (b % 2 == 0) {
+            return val * val % MOD;
+        } else {
+            return val * (val * a % MOD) % MOD;
+
+        }
+    }
+
+    static class Scanner {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public Scanner() throws FileNotFoundException {
+            // System.setOut(new PrintStream(new
+            // BufferedOutputStream(System.out), true));
+            br = new BufferedReader(new InputStreamReader(System.in));
+            // br = new BufferedReader(new InputStreamReader(new
+            // FileInputStream(new File("input.txt"))));
+        }
+
+        public String next() {
+
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    throw new RuntimeException();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            st = null;
+            try {
+                return br.readLine();
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+        }
+
+        public boolean endLine() {
+            try {
+                String next = br.readLine();
+                while (next != null && next.trim().isEmpty()) {
+                    next = br.readLine();
+                }
+                if (next == null) {
+                    return true;
+                }
+                st = new StringTokenizer(next);
+                return st.hasMoreTokens();
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+        }
+    }
+}
