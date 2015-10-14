17a39f9ca1576b676a67bf3bd7f9c6f642223d90
trungpham90 <trungpham90@gmail.com>
2015-10-15 04:31:46 +0800

    F Bubble cup 8 final

---
+++ Contest/src/F_BubbleCup_Final.java
@@ -0,0 +1,339 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ * #
+ * @author pttrung
+ */
+public class F_BubbleCup_Final {
+
+    public static long MOD = 1000000007;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // PrintWriter out = new PrintWriter(new FileOutputStream(new File(
+        // "output.txt")));
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner in = new Scanner();
+        int n = in.nextInt();
+        int x = in.nextInt();
+        Point[] data = new Point[n + 1];
+        data[0] = new Point(x, x);
+
+        TreeSet<Integer> set = new TreeSet();
+        set.add(x);
+        for (int i = 1; i <= n; i++) {
+            data[i] = new Point(in.nextInt(), in.nextInt());
+            set.add(data[i].x);
+            set.add(data[i].y);
+        }
+        HashMap<Integer, Integer> map = new HashMap();
+        int index = 0;
+        ArrayList<Integer> pos = new ArrayList(set);
+        for (int i : set) {
+            map.put(i, index++);
+        }
+        long[][] root = new long[2][index];
+        int start = map.get(data[n - 1].x);
+        int end = map.get(data[n - 1].y);
+        long[][] fast = new long[2][index];
+        for (int i = index - 1; i >= 0; i--) {
+
+            int v = pos.get(i);
+            //  System.out.println(map.containsKey(v) + " " + v);
+            if (data[n].x <= v && v <= data[n].y) {
+                root[0][i] = 0;
+            } else {
+                root[0][i] = min(abs(v - data[n].x), abs(v - data[n].y));
+            }
+            if (i <= start) {
+                fast[0][i] = root[0][i];
+                if (i + 1 <= start) {
+                    fast[0][i] = min(fast[0][i], fast[0][i + 1]);
+                }
+            }
+
+        }
+        for (int i = end; i < index; i++) {
+            fast[0][i] = root[0][i];
+            if (i - 1 >= end) {
+                fast[0][i] = min(fast[0][i], fast[0][i - 1]);
+            }
+        }
+        int cur = 1;
+        for (int i = n - 1; i >= 0; i--) {
+
+            start = i - 1 >= 0 ? map.get(data[i - 1].x) : -1;
+            end = i - 1 >= 0 ? map.get(data[i - 1].y) : index;
+
+            for (int j = index - 1; j >= 0; j--) {
+                int v = pos.get(j);
+                long tmp;
+                if (data[i].x <= v && v <= data[i].y) {
+                    root[cur][j] = root[1 - cur][j];
+                } else {
+                    long a = abs(data[i].x - v);
+                    long b = abs(data[i].y - v);
+                    if (a < b) {
+                        tmp = a + fast[1 - cur][j];
+                    } else {
+                        tmp = b + fast[1 - cur][j];
+                    }
+                    root[cur][j] = tmp;
+                }
+                if (j <= start) {
+                    fast[cur][j] = root[cur][j];
+                    if (j + 1 <= start) {
+                        fast[cur][j] = min(fast[cur][j], fast[cur][j + 1]);
+                    }
+                }
+            }
+            for (int j = end; j < index; j++) {
+                fast[cur][j] = root[cur][j];
+                if (j - 1 >= end) {
+                    fast[cur][j] = min(fast[cur][j], fast[cur][j - 1]);
+                }
+            }
+            cur = 1 - cur;
+        }
+        out.println(root[1 - cur][map.get(x)]);
+        out.close();
+    }
+
+    static long min(long a, long b) {
+        return a < b ? a : b;
+    }
+
+    static long abs(long v) {
+        return v < 0 ? -v : v;
+    }
+
+    public static int[] KMP(String val) {
+        int i = 0;
+        int j = -1;
+        int[] result = new int[val.length() + 1];
+        result[0] = -1;
+        while (i < val.length()) {
+            while (j >= 0 && val.charAt(j) != val.charAt(i)) {
+                j = result[j];
+            }
+            j++;
+            i++;
+            result[i] = j;
+        }
+        return result;
+
+    }
+
+    public static boolean nextPer(int[] data) {
+        int i = data.length - 1;
+        while (i > 0 && data[i] < data[i - 1]) {
+            i--;
+        }
+        if (i == 0) {
+            return false;
+        }
+        int j = data.length - 1;
+        while (data[j] < data[i - 1]) {
+            j--;
+        }
+        int temp = data[i - 1];
+        data[i - 1] = data[j];
+        data[j] = temp;
+        Arrays.sort(data, i, data.length);
+        return true;
+    }
+
+    public static int digit(long n) {
+        int result = 0;
+        while (n > 0) {
+            n /= 10;
+            result++;
+        }
+        return result;
+    }
+
+    public static double dist(long a, long b, long x, long y) {
+        double val = (b - a) * (b - a) + (x - y) * (x - y);
+        val = Math.sqrt(val);
+        double other = x * x + a * a;
+        other = Math.sqrt(other);
+        return val + other;
+
+
+
+    }
+
+    public static class Point implements Comparable<Point> {
+
+        int x, y;
+
+        public Point(int start, int end) {
+            this.x = start;
+            this.y = end;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 5;
+            hash = 47 * hash + this.x;
+            hash = 47 * hash + this.y;
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final Point other = (Point) obj;
+            if (this.x != other.x) {
+                return false;
+            }
+            if (this.y != other.y) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return x - o.x;
+        }
+    }
+
+    public static class FT {
+
+        long[] data;
+
+        FT(int n) {
+            data = new long[n];
+        }
+
+        public void update(int index, long value) {
+            while (index < data.length) {
+                data[index] += value;
+                index += (index & (-index));
+            }
+        }
+
+        public long get(int index) {
+            long result = 0;
+            while (index > 0) {
+                result += data[index];
+                index -= (index & (-index));
+            }
+            return result;
+
+        }
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static long pow(long a, long b, long MOD) {
+        if (b == 0) {
+            return 1;
+        }
+        if (b == 1) {
+            return a;
+        }
+        long val = pow(a, b / 2, MOD);
+        if (b % 2 == 0) {
+            return val * val % MOD;
+        } else {
+            return val * (val * a % MOD) % MOD;
+
+
+        }
+    }
+
+    static class Scanner {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public Scanner() throws FileNotFoundException {
+            // System.setOut(new PrintStream(new BufferedOutputStream(System.out), true));
+            br = new BufferedReader(new InputStreamReader(System.in));
+            //  br = new BufferedReader(new InputStreamReader(new FileInputStream(new File("input.txt"))));
+        }
+
+        public String next() {
+
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    throw new RuntimeException();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() {
+            st = null;
+            try {
+                return br.readLine();
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+        }
+
+        public boolean endLine() {
+            try {
+                String next = br.readLine();
+                while (next != null && next.trim().isEmpty()) {
+                    next = br.readLine();
+                }
+                if (next == null) {
+                    return true;
+                }
+                st = new StringTokenizer(next);
+                return st.hasMoreTokens();
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+        }
+    }
+}
