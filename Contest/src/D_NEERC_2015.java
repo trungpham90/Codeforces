f39a8ef5d31496f3eecaa88becb6abd62ffa8527
Pham <thepham@paypal.com>
2015-10-21 13:41:38 +0800

    D NEERC 2015

---
+++ Contest/src/D_NEERC_2015.java
@@ -0,0 +1,311 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ * #
+ * 
+ * @author pttrung
+ */
+public class D_NEERC_2015 {
+
+	public static long MOD = 1000000007;
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// PrintWriter out = new PrintWriter(new FileOutputStream(new File(
+		// "output.txt")));
+		PrintWriter out = new PrintWriter(System.out);
+		Scanner in = new Scanner();
+		int n = in.nextInt();
+		Person[] data = new Person[n];
+		for (int i = 0; i < n; i++) {
+			data[i] = new Person(in.nextInt(), in.nextInt(), in.nextInt());
+		}
+		int[] result = new int[n];
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				Point p1 = new Point(data[i].t, data[i].s);
+				Point q1 = new Point(Math.abs(data[i].f - data[i].s) + data[i].t, data[i].f);
+				Point p2 = new Point(data[j].t, data[j].s);
+				Point q2 = new Point(Math.abs(data[j].f - data[j].s) + data[j].t, data[j].f);
+				//System.out.println(p1 + " " + q1 + " " + p2 + " " + q2);
+				if(doIntersect(p1, q1, p2, q2)){
+					result[i]++;
+					result[j]++;
+				}
+			}
+		}
+		for(int i : result){
+			out.print(i + " ");
+		}
+		out.close();
+	}
+
+	static int orientation(Point p, Point q, Point r) {
+		// See 10th slides from following link for derivation of the formula
+		// http://www.dcs.gla.ac.uk/~pat/52233/slides/Geometry1x1.pdf
+		long val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+
+		if (val == 0)
+			return 0; // colinear
+
+		return (val > 0) ? 1 : 2; // clock or counterclock wise
+	}
+
+	static boolean doIntersect(Point p1, Point q1, Point p2, Point q2) {
+		// Find the four orientations needed for general and
+		// special cases
+		int o1 = orientation(p1, q1, p2);
+		int o2 = orientation(p1, q1, q2);
+		int o3 = orientation(p2, q2, p1);
+		int o4 = orientation(p2, q2, q1);
+
+		// General case
+		if (o1 != o2 && o3 != o4)
+			return true;
+
+		// Special Cases
+		// p1, q1 and p2 are colinear and p2 lies on segment p1q1
+		if (o1 == 0 && onSegment(p1, p2, q1))
+			return true;
+
+		// p1, q1 and p2 are colinear and q2 lies on segment p1q1
+		if (o2 == 0 && onSegment(p1, q2, q1))
+			return true;
+
+		// p2, q2 and p1 are colinear and p1 lies on segment p2q2
+		if (o3 == 0 && onSegment(p2, p1, q2))
+			return true;
+
+		// p2, q2 and q1 are colinear and q1 lies on segment p2q2
+		if (o4 == 0 && onSegment(p2, q1, q2))
+			return true;
+
+		return false; // Doesn't fall in any of the above cases
+	}
+
+	static boolean onSegment(Point p, Point q, Point r) {
+		if (q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x)
+				&& q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y))
+			return true;
+
+		return false;
+	}
+
+	static class Person {
+		int t, s, f;
+
+		public Person(int t, int s, int f) {
+			super();
+			this.t = t;
+			this.s = s;
+			this.f = f;
+		}
+
+	}
+
+	public static int[] KMP(String val) {
+		int i = 0;
+		int j = -1;
+		int[] result = new int[val.length() + 1];
+		result[0] = -1;
+		while (i < val.length()) {
+			while (j >= 0 && val.charAt(j) != val.charAt(i)) {
+				j = result[j];
+			}
+			j++;
+			i++;
+			result[i] = j;
+		}
+		return result;
+
+	}
+
+	public static boolean nextPer(int[] data) {
+		int i = data.length - 1;
+		while (i > 0 && data[i] < data[i - 1]) {
+			i--;
+		}
+		if (i == 0) {
+			return false;
+		}
+		int j = data.length - 1;
+		while (data[j] < data[i - 1]) {
+			j--;
+		}
+		int temp = data[i - 1];
+		data[i - 1] = data[j];
+		data[j] = temp;
+		Arrays.sort(data, i, data.length);
+		return true;
+	}
+
+	public static int digit(long n) {
+		int result = 0;
+		while (n > 0) {
+			n /= 10;
+			result++;
+		}
+		return result;
+	}
+
+	public static double dist(long a, long b, long x, long y) {
+		double val = (b - a) * (b - a) + (x - y) * (x - y);
+		val = Math.sqrt(val);
+		double other = x * x + a * a;
+		other = Math.sqrt(other);
+		return val + other;
+
+	}
+
+	public static class Point {
+
+		long x, y;
+
+		public Point(int start, int end) {
+			this.x = start;
+			this.y = end;
+		}
+
+		
+
+		@Override
+		public String toString() {
+			return "Point [x=" + x + ", y=" + y + "]";
+		}
+
+		
+	}
+
+	public static class FT {
+
+		long[] data;
+
+		FT(int n) {
+			data = new long[n];
+		}
+
+		public void update(int index, long value) {
+			while (index < data.length) {
+				data[index] += value;
+				index += (index & (-index));
+			}
+		}
+
+		public long get(int index) {
+			long result = 0;
+			while (index > 0) {
+				result += data[index];
+				index -= (index & (-index));
+			}
+			return result;
+
+		}
+	}
+
+	public static long gcd(long a, long b) {
+		if (b == 0) {
+			return a;
+		}
+		return gcd(b, a % b);
+	}
+
+	public static long pow(long a, long b) {
+		if (b == 0) {
+			return 1;
+		}
+		if (b == 1) {
+			return a;
+		}
+		long val = pow(a, b / 2);
+		if (b % 2 == 0) {
+			return val * val % MOD;
+		} else {
+			return val * (val * a % MOD) % MOD;
+
+		}
+	}
+
+	static class Scanner {
+
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Scanner() throws FileNotFoundException {
+			// System.setOut(new PrintStream(new
+			// BufferedOutputStream(System.out), true));
+			br = new BufferedReader(new InputStreamReader(System.in));
+			// br = new BufferedReader(new InputStreamReader(new
+			// FileInputStream(new File("input.txt"))));
+		}
+
+		public String next() {
+
+			while (st == null || !st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (Exception e) {
+					throw new RuntimeException();
+				}
+			}
+			return st.nextToken();
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		public String nextLine() {
+			st = null;
+			try {
+				return br.readLine();
+			} catch (Exception e) {
+				throw new RuntimeException();
+			}
+		}
+
+		public boolean endLine() {
+			try {
+				String next = br.readLine();
+				while (next != null && next.trim().isEmpty()) {
+					next = br.readLine();
+				}
+				if (next == null) {
+					return true;
+				}
+				st = new StringTokenizer(next);
+				return st.hasMoreTokens();
+			} catch (Exception e) {
+				throw new RuntimeException();
+			}
+		}
+	}
+}
